665a04bc8046d4474f9b23e151f5eccfde6cf1bc
Shovan-Banik <shovanbanik3@gmail.com>
2021-06-27 01:01:02 +0600

    add new three code

---
+++ DOM(document object model) for access element/index.js
@@ -0,0 +1,9 @@
+//These are include in DOM(document object model)
+document.getElementById("head1").innerHTML="Game lover shovan";
+//getElementById sudhu matro akta element niye kaj kore 
+document.getElementsByClassName("head2")[0].innerHTML="view pai na";
+//getElementsByClassName akadhik elements niye kaj kore and array akare value return kore
+//jar jonno ai method diye kono element access korte chaile index[] value dite hy
+document.getElementsByTagName("h1")[0].innerHTML="pok pok";
+//getElementsByTagName...o akadhik elements niye kaj kore and array akare value return kore
+//jar jonno ai method diye...o kono element access korte chaile index[] value dite hy
