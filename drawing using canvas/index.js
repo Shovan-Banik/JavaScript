cf2659d9348152c89f7108ef09a39e889ecb2d10
Shovan-Banik <shovanbanik3@gmail.com>
2021-07-09 01:52:37 +0600

    Canva in javascript

--- drawing using canvas/index.js
+++ drawing using canvas/index.js
@@ -1,18 +1,20 @@
 var canva=document.getElementById("myCanvas");
 var can=canva.getContext("2d");
 
-can.lineWidth=3;
-can.strokeStyle="black";
-can.strokeRect(10,10,382,282);
+can.lineWidth=3;//border koto toko mota hbe
+can.strokeStyle="black";//border color
+can.strokeRect(10,10,382,282);//border ta kothay hbe
 
-can.fillStyle="green";
-can.fillRect(11,11,380,280);
+can.fillStyle="green";// vitorer ki color diye fill hbe
+can.fillRect(11,11,380,280);// rectangle tar size kmn hbe
 
-var centerX=canva.width / 2;
-var centerY=canva.height / 2;
+var centerX=canva.width / 2;//x axis ar middle point ber kora 
+var centerY=canva.height / 2;//y axis ar middle point ber kora 
 
-can.beginPath();
+can.beginPath();// kotha theke soro hbe
 can.arc(centerX,centerY,100,0,2*Math.PI,false);
-can.fillStyle="red";
-can.fill();
-can.stroke();
+//false diye bujano hoyece anti clock nki clock wise
+// 0 diye bujano hyece middle point theke soro korte
+can.fillStyle="red";// circle ar color ki hbe
+can.fill(); // fill korte bola hoyece red color diye
+can.stroke(); // circle ar sobdike border dite bola hyece..oporer stroke diclare kora ace
